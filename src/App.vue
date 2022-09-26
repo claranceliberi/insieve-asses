8c98409e1283a726f7b9ee7a61ca7eeec9bae158
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2022-09-26 23:02:26 +0200

    feat: archive and read

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,170 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import TheButton from "./components/atoms/TheButton.vue";
-import CheckBox from "./components/atoms/CheckBox.vue";
+import { mapStores } from "pinia";
+import { useEmailStore } from "./stores/email";
 
 export default defineComponent({
   components: {
     TheButton,
-    CheckBox,
   },
 
   data() {
     return {
+      active: "",
       checked: false,
+      selectAll: false,
+      selected: [] as number[],
     };
+  },
+  watch: {
+    selectAll(val) {
+      if (val) this.selected = this.emailStore.inbox.map((email) => email.id);
+      else this.selected = [];
+    },
+  },
+  methods: {
+    changeTab(tab: string) {
+      if (this.active === tab) return;
+      this.active = tab;
+      this.selected = [];
+    },
+  },
+  computed: {
+    ...mapStores(useEmailStore),
   },
 });
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">localhost</div>
-  </header>
-
-  <main>
-    <TheButton> button</TheButton>
-    <TheButton appearance="text"> button</TheButton>
-    <CheckBox id="checkbox" v-model="checked" />
-    {{ checked }}
-  </main>
+  <div class="main">
+    <aside class="aside">
+      <div class="aside--top">
+        <TheButton @click="changeTab('inbox')"> Inbox</TheButton>
+        <TheButton @click="changeTab('archive')"> Archived</TheButton>
+      </div>
+      <div class="aside-bottom">
+        <TheButton> Logout</TheButton>
+      </div>
+    </aside>
+    <section class="inbox" v-if="active === 'inbox'">
+      <header>
+        <h5>{{ active }}</h5>
+        <h1>Email Selected ({{ emailStore.inbox.length }})</h1>
+      </header>
+      <main>
+        <div>
+          <input type="checkbox" id="select-all" v-model="selectAll" />
+          <TheButton @click="emailStore.readEmails(selected)"
+            >Mark as Read (r)
+          </TheButton>
+          <TheButton @click="emailStore.archiveEmails(selected)"
+            >Archive (a)</TheButton
+          >
+        </div>
+        <div>
+          <div
+            class="email"
+            :class="{ email__read: email.read }"
+            v-for="email in emailStore.inbox"
+            :key="email.id"
+          >
+            <input
+              class="email--checkbox"
+              type="checkbox"
+              :id="'email' + email.id"
+              v-model="selected"
+              :value="email.id"
+            />
+            <h2 class="email--title">{{ email.title }}</h2>
+          </div>
+        </div>
+      </main>
+    </section>
+    <section class="archive" v-else>
+      <header>
+        <h5>{{ active }}</h5>
+        <h1>Email Selected ({{ emailStore.archived.length }})</h1>
+      </header>
+      <main>
+        <div>
+          <input type="checkbox" id="select-all" v-model="selectAll" />
+          <TheButton @click="emailStore.unarchiveEmails(selected)"
+            >Unarchive (u)</TheButton
+          >
+        </div>
+        <div>
+          <div
+            class="email"
+            :class="{ email__read: email.read }"
+            v-for="email in emailStore.archived"
+            :key="email.id"
+          >
+            <input
+              class="email--checkbox"
+              type="checkbox"
+              :id="'email' + email.id"
+              v-model="selected"
+              :value="email.id"
+            />
+            <h2 class="email--title">{{ email.title }}</h2>
+          </div>
+        </div>
+      </main>
+    </section>
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.main {
+  display: flex;
+  padding: 0.5rem 1rem;
+  background: black;
+  height: 100vh;
+  .aside {
+    width: 20%;
+    padding: 0.5rem 1rem;
+    border-radius: 2px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    &--top {
+      display: flex;
+      flex-direction: column;
+      gap: 0.5rem;
+    }
+    &--bottom {
+      margin-top: auto;
+    }
+  }
+  .inbox,
+  .archive {
+    background: white;
+    flex-grow: 1;
+    display: flex;
+    padding-left: 1rem;
+    flex-direction: column;
+    .email {
+      display: flex;
+      padding: 0.4rem 1rem;
+      background-color: #cccccc;
+      margin-bottom: 0.5rem;
+      margin-top: 0.5rem;
+      border-radius: 5px;
+      border: 1px solid #f4f0f9;
+
+      &__read {
+        opacity: 0.3;
+      }
+
+      &--checkbox {
+        margin-right: 1rem;
+      }
+      &--title {
+        margin: 0;
+        font-size: 1.1rem;
+      }
+    }
+  }
+}
+</style>
--- src/stores/email.ts
+++ src/stores/email.ts
@@ -3,7 +3,43 @@ import type { Email } from "@/types";
 
 export const useEmailStore = defineStore("email", {
   state: () => ({
-    email: [] as Email[],
+    email: [
+      {
+        id: 0,
+        title: "Email 0",
+        body: "This is email 0",
+        read: false,
+        archived: false,
+      },
+      {
+        id: 1,
+        title: "Email 1",
+        body: "This is email 1",
+        read: true,
+        archived: false,
+      },
+      {
+        id: 2,
+        title: "Email 2",
+        body: "This is email 2",
+        read: false,
+        archived: false,
+      },
+      {
+        id: 3,
+        title: "Email 3",
+        body: "This is email 3",
+        read: false,
+        archived: false,
+      },
+      {
+        id: 4,
+        title: "Email 4",
+        body: "This is email 4",
+        read: false,
+        archived: true,
+      },
+    ] as Email[],
   }),
   getters: {
     inbox: (state) => state.email.filter((email) => !email.archived),
@@ -15,6 +51,18 @@ export const useEmailStore = defineStore("email", {
       if (email) {
         email.archived = true;
       }
+    },
+    archiveEmails(ids: number[]) {
+      ids.forEach((id) => this.archiveEmail(id));
+    },
+    unarchiveEmail(id: number) {
+      const email = this.email.find((email) => email.id === id);
+      if (email) {
+        email.archived = false;
+      }
+    },
+    unarchiveEmails(ids: number[]) {
+      ids.forEach((id) => this.unarchiveEmail(id));
     },
     readEmail(id: number) {
       const email = this.email.find((email) => email.id === id);
